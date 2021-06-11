7ca61ff16bfa413ef646ba356021f0d8e0299da3
gergaczd <gergaczdaniel@gmail.com>
2021-06-11 15:21:31 +0200

    show in-progress progress on circle chart

--- src/components/ObjectiveChart.vue
+++ src/components/ObjectiveChart.vue
@@ -13,6 +13,12 @@
         class="global-objectives__chart"
         type="radialBar"
         height="100%"
+        :options="inProgressOptions"
+        :series="objectiveInProgress"></apexchart>
+      <apexchart
+        class="global-objectives__chart"
+        type="radialBar"
+        height="100%"
         :options="options1"
         :series="objectiveProgresses"></apexchart>
     </div>
@@ -139,6 +145,52 @@ export default {
         }
       },
 
+      inProgressOptionsDefault: {
+        chart: {},
+        plotOptions: {
+          radialBar: {
+            offsetY: 0,
+            startAngle: -50,
+            endAngle: 230,
+            track: {
+              show: true,
+              startAngle: undefined,
+              endAngle: undefined,
+              background: "#11264a",
+              strokeWidth: "0%",
+              opacity: 1,
+              margin: 5
+            },
+            hollow: {
+              margin: 50,
+              size: "35%",
+              background: "transparent",
+              image: undefined,
+              dropShadow: {
+                enabled: false,
+                top: 0,
+                left: 0,
+                blur: 3,
+                opacity: 1
+              }
+            },
+            dataLabels: {
+              name: {
+                show: false
+              },
+              value: {
+                show: false
+              }
+            }
+          }
+        },
+        colors: ['#d3dc2d', '#56c5c9', '#6879ae', '#3a5684', '#40877e', '#87991a', '#efab00', '#d8d8d8'],
+        fill: {
+          opacity: 0.4
+        }
+      },
+
+
       series: [40, 80, 60, 80, 56, 65, 55],
       series2: [100, 90, 85, 85, 80, 78, 75]
     };
@@ -154,17 +206,20 @@ export default {
       const sortedObjectives = sortObjectives(this.objectives)
       const headObjectives = sortedObjectives.slice(0, maxObjectivesOnPage - 1)
       const tailObjectives = sortedObjectives.slice(maxObjectivesOnPage - 1)
-      const defaultOtherObjective = { name: 'Other Projects', weeks: 0, weeksDone: 0, progress: 0 }
+      const defaultOtherObjective = { name: 'Other Projects', weeks: 0, weeksDone: 0, weeksInProgress: 0 }
 
       const summarizedOtherObjective = tailObjectives.reduce((acc, obj) => {
         acc.weeks += obj.weeks
         acc.weeksDone += obj.weeksDone
-        acc.progress += obj.progress
+        acc.weeksInProgress += obj.weeksInProgress
         return acc
       }, defaultOtherObjective)
 
       const progressRate = summarizedOtherObjective.weeksDone / summarizedOtherObjective.weeks;
       summarizedOtherObjective.progress = Math.round(progressRate * 100) || 0;
+
+      const inprogressRate = (summarizedOtherObjective.weeksDone + summarizedOtherObjective.weeksInProgress) / summarizedOtherObjective.weeks;
+      summarizedOtherObjective.progressWithInProgress = Math.round(inprogressRate * 100) || 0;
 
       return sortObjectives(headObjectives.concat(summarizedOtherObjective))
     },
@@ -181,6 +236,12 @@ export default {
       return options;
     },
 
+    inProgressOptions() {
+      let options = deepCopy(this.inProgressOptionsDefault)
+      if (this.summarizedObjectives.length > 4) options.plotOptions.radialBar.hollow.size = '15%'
+      return options;
+    },
+
 
     objectivesWithRelativeValues() {
       let objectives = this.summarizedObjectives.map(objective => {
@@ -188,7 +249,8 @@ export default {
           name: objective.name,
           weeks: objective.weeks,
           weeksDone: objective.weeksDone,
-          progress: objective.progress
+          progress: objective.progress,
+          progressWithInProgress: objective.progressWithInProgress
         }
       })
 
@@ -204,6 +266,7 @@ export default {
 
         if (trackLength > 100) trackLength = 100;
         let progressOnTrack = Math.ceil((objective.progress / 100) * trackLength) || 0;
+        let inprogressOnTrack = Math.ceil((objective.progressWithInProgress / 100) * trackLength) || 0;
 
         return {
           name: objective.name,
@@ -211,7 +274,8 @@ export default {
           weeksDone: objective.weeksDone,
           progress: objective.progress,
           trackLength,
-          progressOnTrack
+          progressOnTrack,
+          inprogressOnTrack
         }
       })
     },
@@ -225,6 +289,12 @@ export default {
     objectiveProgresses() {
       return this.objectivesWithRelativeValues.map(
         objective => objective.progressOnTrack
+      )
+    },
+
+    objectiveInProgress() {
+      return this.objectivesWithRelativeValues.map(
+        objective => objective.inprogressOnTrack
       )
     }
   },
